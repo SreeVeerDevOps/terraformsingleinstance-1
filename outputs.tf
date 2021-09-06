80ef8f2ce1b424014bb20eb321248fd1e63bd380
SreeVeerDevOps <47476473+SreeVeerDevOps@users.noreply.github.com>
2021-09-06 12:13:52 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,9 @@
-output "id" {
-  description = "List of IDs of instances"
-  value       = ["${aws_instance.web-1.*.id}"]
-}
+# output "id" {
+#   description = "List of IDs of instances"
+#   value       = ["${aws_instance.web-1.*.id}"]
+# }
 
-output "availability_zone" {
-  description = "List of availability zones of instances"
-  value       = ["${aws_instance.web-1.*.availability_zone}"]
-}
-
-k
+# output "availability_zone" {
+#   description = "List of availability zones of instances"
+#   value       = ["${aws_instance.web-1.*.availability_zone}"]
+# }
