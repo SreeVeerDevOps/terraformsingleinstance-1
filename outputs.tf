a36bdf376a793d9d15eef78ed05d5130be669c4b
root <sreeharshav>
2021-09-06 06:35:11 +0000

    Deleted Files

---
+++ outputs.tf
@@ -0,0 +1,11 @@
+output "id" {
+  description = "List of IDs of instances"
+  value       = ["${aws_instance.web-1.*.id}"]
+}
+
+output "availability_zone" {
+  description = "List of availability zones of instances"
+  value       = ["${aws_instance.web-1.*.availability_zone}"]
+}
+
+k
