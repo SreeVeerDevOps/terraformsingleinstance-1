7bac5d6b09a680d337a7551b8a7bfe355836439a
SreeVeerDevOps <47476473+SreeVeerDevOps@users.noreply.github.com>
2021-09-06 12:12:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -90,19 +90,17 @@ resource "aws_security_group" "allow_all" {
     }
 }
 
-resource "aws_instance" "web-1" {
+resource "aws_instance" "public-servers" {
+    count = 3
     ami = "ami-00d1ab6b335f217cf"
     availability_zone = "us-east-1a"
-    instance_type = "t2.micro"
+    instance_type = "t2.nano"
     key_name = "LaptopKey"
     subnet_id = "${aws_subnet.subnet1-public.id}"
     vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
     associate_public_ip_address = true	
     tags = {
-        Name = "Server-1"
-        Env = "Prod"
-        Owner = "Sree"
-	CostCenter = "ABCD"
+        Name = "Server-${count.index+1}"
     }
 }
 
